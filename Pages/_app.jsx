2272c0855bef18df7491bcfbc2cc0b097df1eaaa
kimminkyu <elliotelon@gmail.com>
2022-03-07 22:47:31 +0900

    공통 문서 만들기

--- Pages/_app.jsx
+++ Pages/_app.jsx
@@ -9,6 +9,7 @@ const MyApp = ({ Component, pageProps }) => {
         {`
           body {
             margin: 0;
+            font-family: Noto Sans, Noto Sans KR;
           }
         `}
       </style>
