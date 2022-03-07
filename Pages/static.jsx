645964c902ecc199fe840af4ac4e45f3920f2f51
kimminkyu <elliotelon@gmail.com>
2022-03-07 14:22:27 +0900

    깃허브 프로필 스타일링

--- Pages/static.jsx
+++
@@ -1,9 +0,0 @@
-const staticPage = ({ time }) => {
-  return <div>{time}</div>;
-};
-
-export const getStaticProps = async () => {
-  return { props: { time: new Date().toISOString() }, revalidate: 3 };
-};
-
-export default staticPage;
--- Pages/users/[name].jsx
+++ Pages/users/[name].jsx
@@ -1,9 +1,58 @@
+import css from "styled-jsx/css";
 import fetch from "isomorphic-unfetch";
 
-const name = ({ user }) => {
-  const username = user && user.name;
-  return <div>{username}</div>;
-};
+const style = css`
+.profile-box {
+  width: 25%;
+  max-width: 272px;
+  margin-right: 26px;
+}
+.profile-image-wrapper {
+  width: 100%;
+  border: 1px solid #e1e4e8;
+}
+.profile-image-wrapper .profile-image {
+  display: block;
+  width: 100%;
+}
+.profile-username {
+  margin: 0;
+  padding-top: 16px;
+  font-size: 26px
+}
+.profile-user-login {
+  margin: 0;
+  font-size: 20px;
+}
+.profile-user-bio {
+  margin: 0;
+  padding-top: 16px;
+  font-size: 14px;
+}
+`
+
+const name = ({user}) => {
+  if(!user) {
+    return null;
+  }
+  return (
+    <>
+      <div className="profile-box">
+        <div className="profile-image-wrapper">
+          <img
+            className="profile-image"
+            src={user.avatar_url}
+            alt={`${user.name}`}
+          />
+        </div>
+        <h2 className="profile-username">{user.name}</h2>
+        <p className="profile-user-login">{user.login}</p>
+        <p className="profile-user-bio">{user.bio}</p>
+      </div>
+      <style jsx>{style}</style>
+    </>
+  )
+}
 
 export const getServerSideProps = async ({ query }) => {
   const { name } = query;
